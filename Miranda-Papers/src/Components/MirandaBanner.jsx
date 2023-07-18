c8b293a64a5a85285cdaef77344a378d36f58dd1
Mzwandile <mzwandilethatojunior@gmail.com>
2023-07-18 22:43:51 +0200

    project update

--- Miranda-Papers/src/Components/MirandaBanner.jsx
+++ Miranda-Papers/src/Components/MirandaBanner.jsx
@@ -1,7 +1,10 @@
 const MirandaBanner = () => {
   return (
-    <div id="fullPageBanner" className="bg-[#1C1C1A] w-[95%] mx-auto my-10">
-      <h1 className="text-white text-center font-Canopee py-44 text-[500px]">
+    <div
+      id="fullPageBanner"
+      className="bg-[#1C1C1A] w-[96%] mx-auto my-10 tracking-[-1.8em]"
+    >
+      <h1 className="text-[#C6BFB5]  font-Canopee py-56 text-[570px]">
         MIRANDA
       </h1>
     </div>
